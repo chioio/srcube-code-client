da608c30b20bbcc63246fcf6e2ab7c2fa6ddba45
Tenn Chio <Tenn_Chio@icloud.com>
2022-03-30 03:54:23 +0800

    update: graphql schema types

--- lib/api/schema.ts
+++ lib/api/schema.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -7,133 +8,143 @@
 /* tslint:disable */
 /* eslint-disable */
 export enum UserRole {
-  ROOT = 'ROOT',
-  ADMIN = 'ADMIN',
-  USER = 'USER',
+    ROOT = "ROOT",
+    ADMIN = "ADMIN",
+    USER = "USER"
 }
 
 export enum AccountType {
-  USERNAME = 'USERNAME',
-  EMAIL = 'EMAIL',
+    USERNAME = "USERNAME",
+    EMAIL = "EMAIL"
 }
 
 export interface ExistedCheckInput {
-  type: AccountType
-  value: string
+    type: AccountType;
+    value: string;
 }
 
 export interface CreateUserInput {
-  username: string
-  password: string
-  firstName: string
-  lastName: string
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    avatar: string;
 }
 
 export interface UpdateUserInput {
-  username?: Nullable<string>
-  password?: Nullable<string>
-  firstName?: Nullable<string>
-  lastName?: Nullable<string>
-  id: number
+    username?: Nullable<string>;
+    password?: Nullable<string>;
+    firstName?: Nullable<string>;
+    lastName?: Nullable<string>;
+    avatar?: Nullable<string>;
+    id: number;
 }
 
 export interface SignInInput {
-  account: string
-  password: string
-  type: string
+    account: string;
+    password: string;
+    type: string;
 }
 
 export interface SignUpInput {
-  firstName: string
-  lastName: string
-  email: string
-  username: string
-  password: string
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    password: string;
 }
 
-export interface CreateWorkInput {
-  name?: Nullable<string>
-  username: string
-  codeHTML: string
-  codeCSS: string
-  codeJS: string
+export interface CreateCreationInput {
+    title?: Nullable<string>;
+    author: string;
+    code?: Nullable<CreationCodeInput>;
 }
 
-export interface UpdateWorkInput {
-  name?: Nullable<string>
-  username?: Nullable<string>
-  codeHTML?: Nullable<string>
-  codeCSS?: Nullable<string>
-  codeJS?: Nullable<string>
-  id: number
+export interface CreationCodeInput {
+    html?: Nullable<string>;
+    css?: Nullable<string>;
+    javascript?: Nullable<string>;
+}
+
+export interface UpdateCreationInput {
+    title?: Nullable<string>;
+    author?: Nullable<string>;
+    code?: Nullable<CreationCodeInput>;
+    id: number;
 }
 
 export interface User {
-  email: string
-  username: string
-  password: string
-  nickname?: Nullable<string>
-  firstName: string
-  lastName: string
-  roles: UserRole[]
-  createdAt: DateTime
-  updatedAt: DateTime
+    email: string;
+    username: string;
+    password: string;
+    nickname?: Nullable<string>;
+    firstName: string;
+    lastName: string;
+    avatar: string;
+    roles: UserRole[];
+    createdAt: DateTime;
+    updatedAt: DateTime;
 }
 
 export interface ExistedCheckOutput {
-  result: boolean
+    result: boolean;
 }
 
 export interface UserProfileOutput {
-  username: string
-  nickname?: Nullable<string>
-  firstName?: Nullable<string>
-  lastName?: Nullable<string>
-  avatar: string
-  roles: UserRole[]
+    username: string;
+    nickname?: Nullable<string>;
+    firstName: string;
+    lastName: string;
+    avatar: string;
+    roles: UserRole[];
 }
 
 export interface SignInOutput {
-  token: string
-  profile: UserProfileOutput
+    token: string;
+    profile: UserProfileOutput;
 }
 
 export interface SignOutOutput {
-  msg: string
+    msg: string;
 }
 
 export interface SignUpOutput {
-  user: string
+    user: string;
 }
 
-export interface Work {
-  name: string
-  user: string
-  codeHTML: string
-  codeCSS: string
-  codeJS: string
-  createdAt: DateTime
-  updatedAt: DateTime
+export interface CreationCode {
+    html: string;
+    css: string;
+    javascript: string;
+}
+
+export interface Creation {
+    _id: string;
+    title: string;
+    author: string;
+    code: CreationCode;
+    createdAt: DateTime;
+    updatedAt: DateTime;
 }
 
 export interface IQuery {
-  users(): User[] | Promise<User[]>
-  user(username: string): User | Promise<User>
-  whoAmI(): User | Promise<User>
-  existedCheck(input: ExistedCheckInput): ExistedCheckOutput | Promise<ExistedCheckOutput>
-  work(id: number): Work | Promise<Work>
+    users(): User[] | Promise<User[]>;
+    user(username: string): User | Promise<User>;
+    whoAmI(): User | Promise<User>;
+    existedCheck(input: ExistedCheckInput): ExistedCheckOutput | Promise<ExistedCheckOutput>;
+    creation(id: number): Creation | Promise<Creation>;
 }
 
 export interface IMutation {
-  createUser(createUserInput: CreateUserInput): User | Promise<User>
-  updateUser(updateUserInput: UpdateUserInput): User | Promise<User>
-  signIn(input: SignInInput): SignInOutput | Promise<SignInOutput>
-  signOut(): SignOutOutput | Promise<SignOutOutput>
-  signUp(input: SignUpInput): SignUpOutput | Promise<SignUpOutput>
-  createWork(createWorkInput: CreateWorkInput): Work | Promise<Work>
-  updateWork(updateWorkInput: UpdateWorkInput): Work | Promise<Work>
-  removeWork(id: number): Work | Promise<Work>
+    createUser(createUserInput: CreateUserInput): User | Promise<User>;
+    updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+    signIn(input: SignInInput): SignInOutput | Promise<SignInOutput>;
+    signOut(): SignOutOutput | Promise<SignOutOutput>;
+    signUp(input: SignUpInput): SignUpOutput | Promise<SignUpOutput>;
+    createCreation(input: CreateCreationInput): Creation | Promise<Creation>;
+    updateCreation(input: UpdateCreationInput): Creation | Promise<Creation>;
+    removeCreation(id: number): Creation | Promise<Creation>;
 }
 
-export type DateTime = any
-type Nullable<T> = T | null
+export type DateTime = any;
+type Nullable<T> = T | null;
